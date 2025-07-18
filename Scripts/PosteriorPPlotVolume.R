7f5ea341bc9a2c7f3d423cb39209f2b8f27fc75b
Eiren Jacobson <eiren.jacobson@gmail.com>
2025-07-18 10:54:35 +0100

    Scripts to create prior and posterior predictive plots for volume

---
+++ Scripts/PosteriorPPlotVolume.R
@@ -0,0 +1,16 @@
+
+# NOTE requires running detection_decomp_MMs_real_data_with_miFish_and_NCOG_depthAndSiteCombo.R
+
+mm.volsummary <- mm.data %>%
+  group_by(Volume_filt_mL) %>%
+  summarize(P = sum(delphinus_all)/length(delphinus_all)) %>%
+  mutate(PCorr = P/max(P))
+
+# Create 4-panel plot for site x depth model
+ggplot(plot_data_capture_vol, aes(x = volume, y = capture)) +
+#  geom_boxplot(data = mm.depthsummary, aes(x=Depth_round, y= PCorr, group = Depth_round)) +
+  geom_point(data = mm.data, aes(x=Volume_filt_mL, y= delphinus_all))+
+  stat_lineribbon(alpha = 0.25, fill = "#4CAF50", color = "#2E7D32", 
+                  .width = c(0.25, 0.5, 0.75)) +
+  labs(x = "Volume (mL)", y = "Probability of Capture", title = "Volume: Volume Effect on Capture w/ PPC") +
+  theme_minimal()
---
+++ Scripts/PriorPPlotVolume.R
@@ -0,0 +1,36 @@
+# Script to plot prior predictive checks for volume
+# NOTE requires running detection_decomp_MMs_real_data_with_miFish_and_NCOG_depthAndSiteCombo.R
+
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(boot)
+
+mm.volsummary <- mm.data %>%
+  group_by(Volume_filt_mL) %>%
+  summarize(P = sum(delphinus_all)/length(delphinus_all)) %>%
+  mutate(PCorr = P/max(P))
+
+vol_vals_centered <- seq(-3000, 3000, by = 250)
+
+df <- expand_grid(Cap_prob_site = seq(0, 1, by = 0.1),
+              Vol_Centered = vol_vals_centered, 
+              Method = 1,
+              Betas_vol = seq(-3, 3, by = 0.25)) %>%
+  mutate(Pred = inv.logit(Cap_prob_site + Vol_Centered*Betas_vol)) %>%
+  mutate(Group = paste0(Betas_vol, "_", Cap_prob_site))
+
+ggplot(filter(df)) +
+  geom_line(aes(x= Vol_Centered+3000, y = Pred, group = Group), col = "gray") +
+  geom_point(data = mm.data, aes(x=Volume_filt_mL, y= delphinus_all)) +
+  xlab("Volume (mL)") +
+  ylab("Predicted Probability of Occupancy") +
+  theme_minimal()
+
+ggplot(filter(df)) +
+  geom_line(aes(x= Vol_Centered+3000, y = Pred, group = Group), col = "gray") +
+  geom_boxplot(data = mm.volsummary, aes(x=Volume_filt_mL, y= PCorr, group = Volume_filt_mL)) +
+  scale_y_continuous(name = "Predicted Probability of Occupancy",
+                     sec.axis = sec_axis(~.*1, name="Observed Proportion Occupied")) +
+  xlab("Volume (mL)") +
+  theme_minimal()
