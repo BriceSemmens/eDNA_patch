6154847909c7c2c6d91f33c7833eca776e291d76
Eiren Jacobson <eiren.jacobson@gmail.com>
2025-08-26 15:21:57 +0100

    Updated posterior predictive plots to show aggregated data

--- Scripts/PosteriorPPlotDepth.R
+++ Scripts/PosteriorPPlotDepth.R
@@ -4,7 +4,7 @@
 # source("./detection_decomp_MMs_real_data_with_miFish_and_NCOG_depthAndSiteCombo.R)
 
 mm.depthsummary <- mm.data %>%
-  mutate(Depth_round = round(Depth_m)) %>%
+  mutate(Depth_round = round(Depth_m, digits = -2)) %>%
   # filtered these out bc there were only a couple of obs
 #  filter(Depth_round != 300, Depth_round != 400) %>% 
   group_by(Depth_round) %>%
@@ -25,10 +25,16 @@ ggplot(plot_data_occupancy_depth, aes(x = depth, y = occupancy)) +
 
 # Create plot for site x depth model with observations summarized in bins
 ggplot(plot_data_occupancy_depth, aes(x = depth, y = occupancy)) +
-  geom_boxplot(data = mm.depthsummary, aes(x=Depth_round, y= PPos, group = Depth_round)) +
+
   scale_y_continuous(name = "Predicted Probability of Occupancy",
                      sec.axis = sec_axis(~.*max(mm.depthsummary$P), name="Observed Proportion Occupied")) +
   stat_lineribbon(alpha = 0.25, fill = "#4CAF50", color = "#2E7D32", 
                   .width = c(0.25, 0.5, 0.75)) +
+  geom_point(data = mm.depthsummary, 
+             aes(x=Depth_round, y= PPos, group = Depth_round, size = N), 
+             color = "gray") +
   labs(x = "Depth (m)", title = "Site x Depth: Depth Effect on Occupancy w/ PPC") +
   theme_minimal()
+
+ggsave(plot = last_plot(), file = "./Figures/DepthPosteriorPredCheck.png", 
+       width = 5, height = 4, units = "in")
